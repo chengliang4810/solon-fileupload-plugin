38bb87fc043fd770c98c548ac9489e78d299be1e
chengliang <chengliang4810@163.com>
2024-04-29 18:04:35 +0800

    update

--- layjava-file-plugin-local/src/main/java/com/layjava/solon/file/local/LocalFileStoreService.java
+++ layjava-file-plugin-local/src/main/java/com/layjava/solon/file/local/LocalFileStoreService.java
@@ -1,17 +1,37 @@
 package com.layjava.solon.file.local;
 
+import cn.hutool.core.io.FileUtil;
 import com.layjava.solon.file.FileInfo;
 import com.layjava.solon.file.FileStoreService;
 import org.noear.solon.core.handle.UploadedFile;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * 文件存储服务实现
  */
 public class LocalFileStoreService implements FileStoreService {
 
+    private final String storePath;
+
+    public LocalFileStoreService(String storePath) {
+        this.storePath = storePath;
+    }
+
     @Override
     public FileInfo save(UploadedFile file) {
-        return null;
+        // 不存在则创建的目录
+        File storeDir = FileUtil.file(storePath);
+        if (!storeDir.exists()) {
+            storeDir.mkdirs();
+        }
+        try {
+            file.transferTo(new File(storePath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new FileInfo();
     }
 
 }
--- layjava-file-plugin-local/src/main/java/com/layjava/solon/file/local/XPluginImp.java
+++ layjava-file-plugin-local/src/main/java/com/layjava/solon/file/local/XPluginImp.java
@@ -7,12 +7,19 @@ import org.noear.solon.core.Plugin;
 
 public class XPluginImp implements Plugin {
 
+    /**
+     * 默认上传url
+     */
+    private static final String DEFAULT_UPLOAD_URL = "/file/upload";
+
+
+
     @Override
     public void start(AppContext context) throws Throwable {
         //注册文件存储服务
-        Solon.context().wrapAndPut(LocalFileStoreService.class, new LocalFileStoreService());
+        Solon.context().wrapAndPut(LocalFileStoreService.class, new LocalFileStoreService(""));
         // 注册文件上传接口
-        Solon.app().add("/file/upload", FileUploadController.class);
+        Solon.app().add(DEFAULT_UPLOAD_URL, FileUploadController.class);
     }
 
 }
